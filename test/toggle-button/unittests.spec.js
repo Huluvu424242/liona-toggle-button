d31a4045f4856741fece97ab6ab9bdb75b2c2ffc
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-04-22 12:12:09 +0200

    jest branch

--- test/toggle-button/unittests.spec.js
+++ test/toggle-button/unittests.spec.js
@@ -1,24 +1,25 @@
 const showroom = require('showroom/puppeteer')({headless:true})
 const assert = require('assert')
 
-describe('honey-toggle-button', function() {
+describe('honey-toggle-button', () => {
 
-    before( async function( ) {
-         await showroom.start();
+    beforeAll( async (done) => {
+         await showroom.start().then(()=>done()).catch(done);
     });
 
-    after( async function(  ) {
-         await showroom.stop();
+    afterAll( async (done) => {
+         await showroom.stop().then(()=>done()).catch(done);
     })
 
-    beforeEach( async function(  ) {
-         await showroom.utils.setTestSubject('honey-toggle-button')
+    beforeEach( async (done)=> {
+         await showroom.utils.setTestSubject('honey-toggle-button').then(()=>done()).catch(done);
     })
 
-    it('Prüfe initiale Darstellung', async function( ) {
+    test('Prüfe initiale Darstellung', async (done) => {
         const innerButton =  await showroom.utils.find('// button');
         const text =  await showroom.utils.getTextContent(innerButton);
         assert.equal( text, 'loading...');
+        done();
     })
 
 
