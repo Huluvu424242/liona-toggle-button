16ec61bc92b3b7414fbb63ce51528ec0a5842869
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-04-22 17:07:48 +0200

    imports fixed

--- test/toggle-button/unittests.spec.js
+++ test/toggle-button/unittests.spec.js
@@ -1,25 +1,29 @@
 const showroom = require('showroom/puppeteer')({headless:true})
 const assert = require('assert')
+//const mocha = require('mocha')
 
 describe('honey-toggle-button', () => {
 
-    beforeAll( async (done) => {
-         await showroom.start().then(()=>done()).catch(done);
+    beforeAll( async () => {
+        try{
+            await showroom.start();
+         }catch(e){
+
+         }
     });
 
-    afterAll( async (done) => {
-         await showroom.stop().then(()=>done()).catch(done);
+    afterAll( async () => {
+         await showroom.stop();
     })
 
-    beforeEach( async (done)=> {
-         await showroom.utils.setTestSubject('honey-toggle-button').then(()=>done()).catch(done);
+    beforeEach( async ()=> {
+         await showroom.utils.setTestSubject('honey-toggle-button');
     })
 
-    test('Prüfe initiale Darstellung', async (done) => {
+    test('Prüfe initiale Darstellung', async () => {
         const innerButton =  await showroom.utils.find('// button');
         const text =  await showroom.utils.getTextContent(innerButton);
         assert.equal( text, 'loading...');
-        done();
     })
 
 
