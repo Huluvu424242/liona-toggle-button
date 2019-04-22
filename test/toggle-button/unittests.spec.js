2c4acaa9acfc52fe4da015aa6962b9feccd6afb1
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-04-22 20:06:54 +0200

    asserts erweitert

--- test/toggle-button/unittests.spec.js
+++ test/toggle-button/unittests.spec.js
@@ -1,4 +1,5 @@
 const showroom = require('showroom/puppeteer')({headless:true})
+//const expect = require('expect-puppeteer');
 const assert = require('assert')
 //const mocha = require('mocha')
 
@@ -7,24 +8,47 @@ describe('honey-toggle-button', () => {
     beforeAll( async () => {
         try{
             await showroom.start();
+            console.log('### showroom gestartet: ');
          }catch(e){
-
+            console.log('### exception: '+e);
          }
     });
 
     afterAll( async () => {
-         await showroom.stop();
+        try{
+            await showroom.stop();
+            console.log('### showroom gestoppt: ');
+         }catch(e){
+            console.log('### exception: '+e);
+         }
     })
 
     beforeEach( async ()=> {
-         await showroom.utils.setTestSubject('honey-toggle-button');
+        try{
+            const component = await showroom.setTestSubject('honey-toggle-button');
+             assert.ok( component);
+
+            console.log('### testobject gesetzt');
+        }catch(e){
+            console.log('### exception: '+e);
+        }
     })
 
     test('Prüfe initiale Darstellung', async () => {
+        console.log('### teste...');
+
+        const buttonLoading =  await showroom.find('// button');
+        const text =  await showroom.getTextContent(buttonLoading);
+        assert.strictEqual( text, 'loading...' );
+        const attrToggled = await showroom.getAttribute('toggled');
+        assert.ifError(attrToggled);
+
+
         const innerButton =  await showroom.utils.find('// button');
-        const text =  await showroom.utils.getTextContent(innerButton);
-        assert.equal( text, 'loading...');
+        assert.ok(innerButton);
+        await innerButton.click();
+        const attrToggled1 = await showroom.utils.getAttribute('toggled');
+//        assert.ok(attrToggled1);
+
     })
-
-
 })
