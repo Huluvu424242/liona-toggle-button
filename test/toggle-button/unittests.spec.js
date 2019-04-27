f7b50c7bd4b35ae0674eeb6e3253462bd4581518
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-04-27 22:31:19 +0200

    konkreter pupeteer test scheitert bislang am zugriff auf den shadow root

--- test/toggle-button/unittests.spec.js
+++ test/toggle-button/unittests.spec.js
@@ -1,63 +1,75 @@
-//const showroom = require('showroom/puppeteer')({headless:true})
-//const expect = require('expect-puppeteer');
-//const assert = require('assert')
-//const mocha = require('mocha')
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
 
-// browser,page,context und expect werden deklariert durch die jest-pupeteer umgebung
-
-const timeout = 10000;
-
-describe('honey-toggle-button', () => {
-
-    beforeAll(async () => {
-        await page.setExtraHTTPHeaders({"accept-language": "en-US,en;q=0.9,en-UK;q=0.8,en;q=0.7"});
-        await page.goto('https://google.com');
-    }, timeout);
-
-    afterAll(async () => {
-
-    });
-
-//    beforeEach( async ()=> {
-//        try{
-//            const component = await showroom.setTestSubject('honey-toggle-button');
-//             assert.ok( component);
+////const showroom = require('showroom/puppeteer')({headless:true})
+////const expect = require('expect-puppeteer');
+////const assert = require('assert')
+////const mocha = require('mocha')
 //
-//            console.log('### testobject gesetzt');
-//        }catch(e){
-//            console.log('### exception: '+e);
-//        }
-//    })
-
-    test('should display "google" text on page', async () => {
-        await expect(page).toMatch('Google Inc');
-    }, timeout);
-
-    test('should load without error', async () => {
-      let text = await page.evaluate(() => document.querySelector("input[name='btnI']").getAttribute('aria-label'));
-      expect(text).toContain("I'm Feeling Lucky");
-    })
-
-    test('two plus two is four', () => {
-      expect(2 + 2).toBe(4);
-    });
-
-//    test('Prüfe initiale Darstellung', async () => {
+//// browser,page,context und expect werden deklariert durch die jest-pupeteer umgebung
+//
+//const timeout = 10000;
+//const optionText = "{'content': 'click here to toggle', 'toggledContent': 'click here to untoggle'}";
+//const componentHTML = '<html><honey-toggle-button options="'+optionText+'" toggled="false"></honey-toggle-button></html>';
+//
+//describe('honey-toggle-button', () => {
+//
+//
+//     beforeAll(async () => {
+//        await page.setContent(componentHTML);
+//        const content = await page.content();
+//        console.log('### page-content'+content);
+//     }, timeout);
+//
+//     afterAll(async () => {
+//
+//     });
+//
+//    test('should load without error', async () => {
 //        console.log('### teste...');
 //
-//        const buttonLoading =  await showroom.find('// button');
-//        const text =  await showroom.getTextContent(buttonLoading);
-//        assert.strictEqual( text, 'loading...' );
-//        const attrToggled = await showroom.getAttribute('toggled');
-//        assert.ifError(attrToggled);
+//        const content = await page.content();
+//        console.log('### page-content'+content);
+//
+//        const handle = await page.$('honey-toggle-button');
+//        const text0 = await handle.getProperty('toggled');
+//        expect( text0).toEqual('blah');
+//        let text = await page.evaluate(() => document.querySelector("honey-toggle-button").getProperty('toggled'))
+//        expect(text).toContain("I'm Feeling Lucky");
+//    });
 //
 //
-//        const innerButton =  await showroom.utils.find('// button');
-//        assert.ok(innerButton);
-//        await innerButton.click();
-//        const attrToggled1 = await showroom.utils.getAttribute('toggled');
-////        assert.ok(attrToggled1);
 //
-//    })
-
-}, timeout)
+//
+////    beforeEach( async ()=> {
+////        try{
+////            const component = await showroom.setTestSubject('honey-toggle-button');
+////             assert.ok( component);
+////
+////            console.log('### testobject gesetzt');
+////        }catch(e){
+////            console.log('### exception: '+e);
+////        }
+////    })
+//
+//
+////    test('Prüfe initiale Darstellung', async () => {
+////        console.log('### teste...');
+////
+////        const buttonLoading =  await showroom.find('// button');
+////        const text =  await showroom.getTextContent(buttonLoading);
+////        assert.strictEqual( text, 'loading...' );
+////        const attrToggled = await showroom.getAttribute('toggled');
+////        assert.ifError(attrToggled);
+////
+////
+////        const innerButton =  await showroom.utils.find('// button');
+////        assert.ok(innerButton);
+////        await innerButton.click();
+////        const attrToggled1 = await showroom.utils.getAttribute('toggled');
+//////        assert.ok(attrToggled1);
+////
+////    })
+//
+//}, timeout)
