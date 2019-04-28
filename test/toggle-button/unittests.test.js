f8c441b6490f584d0d35261ddb8801ec34800927
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-04-28 14:30:01 +0200

    Testfälle ergänzt

--- test/toggle-button/unittests.test.js
+++ test/toggle-button/unittests.test.js
@@ -9,14 +9,24 @@ import '../../src/components/toggle-button/ToggleButton.js';
 
 describe('honey toggle-button input', () => {
 
-it('is false by default', async () => {
+  it('toggled is false by default', async () => {
     const el = await fixture(html `<honey-toggle-button options='{ "content": "test", "toggledContent": "blup"}'>leer</honey-toggle-button>`);
     expect(el.toggled).to.be.false;
   });
 
-
-  it('has by default an empty string as label', async () => {
+  it('button was initialized by toggled default with options to content label', async () => {
     const el = /** @type {ToggleButton} */ await fixture(html `<honey-toggle-button options='{"content": "click here to toggle", "toggledContent": "click here to untoggle"}'>leer</honey-toggle-button>`);
     expect(el).shadowDom.to.equal('<button id="toggleButton" toggled="false">click here to toggle</button>');
+  });
+
+
+  it('button was initialized to content label forced by attribut setting toggled to false', async () => {
+    const el = /** @type {ToggleButton} */ await fixture(html `<honey-toggle-button .toggled=${false} options='{"content": "click here to toggle", "toggledContent": "click here to untoggle"}'>leer</honey-toggle-button>`);
+    expect(el).shadowDom.to.equal('<button id="toggleButton" toggled="false">click here to toggle</button>');
+  });
+
+  it('button was initialized to toggledContent label forced by attribut setting toggled to true', async () => {
+    const el = /** @type {ToggleButton} */ await fixture(html `<honey-toggle-button .toggled=${true} options='{"content": "click here to toggle", "toggledContent": "click here to untoggle"}'>leer</honey-toggle-button>`);
+    expect(el).shadowDom.to.equal('<button id="toggleButton" toggled="false">click here to untoggle</button>');
   });
 });
