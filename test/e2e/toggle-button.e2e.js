6837e69345f8ada120e5cfab7ed26b88b2d1c754
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-05-01 23:14:11 +0200

    report schreiben begonnen

--- test/e2e/toggle-button.e2e.js
+++ test/e2e/toggle-button.e2e.js
@@ -2,29 +2,46 @@ const AxeBuilder = require('axe-webdriverjs');
 const WebDriver = require('selenium-webdriver');
 const {getDriver} = require('./support/helpers');
 
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const assert = chai.assert;
+
+
 describe('honey-toggle-button', function () {
 
   let driver;
 
 
-  before(async function () {
+  before(done => {
     driver = getDriver();
+    done();
   });
 
-  after(async function () {
-  })
+  after(done => done());
 
-  beforeEach(async function () {
-  })
+  beforeEach(done => done());
 
-  it('Prüfe initiale Darstellung',   done => {
+  function printReport(results) {
+    if(results.violations.length >0 ) {
+      results.violations.forEach(function (violation) {
+        const id = violation.id;
+        const description = violation.description;
+        console.error(`ERR: \[${id}\] ${description}`);
+      });
+    }
+    assert.lengthOf(results.violations, 0);
+  }
+
+  it('Prüfe initiale Darstellung', done => {
     driver
       .get('http://localhost:3000/simple-component.html')
-      .then(()=> { return AxeBuilder(driver).analyze()
-          .then((results)=> console.log(results))
-          .catch(done);
+      .then(() => AxeBuilder(driver).analyze().then((results) => {
+        printReport(results);
+        done();
       })
-      .then(()=>done())
+        .catch(done))
       .catch(done);
 
     // assert.equal( text, 'loading...');
