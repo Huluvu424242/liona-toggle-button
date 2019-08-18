8e8167adc4f50f70c198c84ee845ded19fcd7aab
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-08-19 00:13:53 +0200

    logging erweitert

--- test/e2e/support/serverHelper.js
+++ test/e2e/support/serverHelper.js
@@ -10,14 +10,20 @@ exports.startServer = function() {
   const webroot = path.join(__dirname, '../sites/')
   const srcroot = path.join(__dirname, '../../../src/')
 
+  console.log('webroot: '+JSON.stringify(webroot));
+  console.log('srcroot: '+JSON.stringify(srcroot));
+
+
+
   const app = express();
   app.use('/', express.static(webroot));
   app.use('/src', express.static(srcroot));
 
   server = http.createServer(app);
+  console.log('server started?: '+JSON.stringify(server));
 
   server.listen(3000, () => {
-    console.log('Express server running at http://localhost:3000');
+    console.log('Express server running at http://localhost:3000/');
   });
 }
 
